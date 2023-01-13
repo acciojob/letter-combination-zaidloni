5d3fc8bb182a7ef9656c144a3cb61ab0a5888b46
Zaid Loni <96578621+zaidloni@users.noreply.github.com>
2023-01-13 10:01:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
+function letterCombinations(input_digit) {
+  //brute force approach
+  var numberStringHt = {
+    2:"abc",
+    3:"def",
+    4:"ghi",
+    5:"jkl",
+    6:"mno",
+    7:"pqrs",
+    8:"tuv",
+    9:"wxyz",
+  }
 
-mappings = {
-  2: ["a", "b", "c"],
-  3: ["d", "e", "f"],
-  4: ["g", "h", "i"],
-  5: ["j", "k", "l"],
-  6: ["m", "n", "o"],
-  7: ["p", "q", "r", "s"],
-  8: ["t", "u", "v"],
-  9: ["w", "x", "y", "z"],
-};
+  var allCombinations = [];
 
-function buildCombi(s) {
-  const ans = s
-    .split("")
-    .map(function (v) {
-      return mappings[v] || [""];
-    })
-    .reduce(function (a, b) {
-      var combi = [];
-      for (var i = 0; i < a.length; i++)
-        for (var j = 0; j < b.length; j++) combi.push(a[i] + b[j]);
-      return combi;
-    });
-  return ans;
+  for (let i = 0; i < input_digit.length; i++) {
+    let firstString = numberStringHt[input_digit[i]] || "";
+    let secondString = numberStringHt[input_digit[i+1]] || "";
+    let thirdString = numberStringHt[input_digit[i+2]] || "";
+
+    if(secondString){
+      for (const firstLetter of firstString) {
+        for (let secondLetter of secondString) {
+          if(input_digit.length == 3){
+            for(let thirdLetter of thirdString){
+              allCombinations.push(firstLetter + secondLetter + thirdLetter)
+            }
+          }else{
+              allCombinations.push(firstLetter + secondLetter)
+          }
+        }
+      }
+    }
+  }
+
+  return allCombinations;
 }
 
 module.exports = letterCombinations;
